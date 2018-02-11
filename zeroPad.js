26f7da5345d4d389b4f1143f76709335cbc61516
Aleksander Molak <aleksander.molak@gmail.com>
2018-02-11 14:30:08 +0100

    Update zeroPad.js
    ES6 update

--- zeroPad.js
+++ zeroPad.js
@@ -1,7 +1,7 @@
 // A simple function working properly only for integers i >= 0
 // It can be useful e.g. when you need to index a variable name ('myVar_001', 'myVar_002', etc.)
 
-zeroPad = function(i, padding) {
+const zeroPad = (i, padding) => {
   if (i.toString().length < padding) {
     return '0'.repeat(padding - i.toString().length) + i
   } else {
